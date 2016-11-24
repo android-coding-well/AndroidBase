6c3ba4427130c7246411d22e4252fc19c5ff2047
huweijian5 <304797017@qq.com>
2016-11-24 21:54:00 +0800

    Initial commit

---
+++ baselibrary/src/main/java/com/junmeng/base/activity/BaseActivity.java
@@ -0,0 +1,294 @@
+package com.junmeng.base.activity;
+
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+
+
+public  class BaseActivity extends AppCompatActivity {
+
+    public Toast longToast;
+    public Toast shortToast;
+    public Context context;
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i("activity_lifecycle", this.getClass() + "  onCreate");
+        super.onCreate(savedInstanceState);
+        context = this;
+        initLoadingDialog();
+        processExtraData();
+    }
+
+    @Override
+    public void recreate() {
+        Log.i("activity_lifecycle", this.getClass() + "  recreate");
+        super.recreate();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i("activity_lifecycle",this.getClass() + "  onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.i("activity_lifecycle", this.getClass() + "  onRestart");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i("activity_lifecycle", this.getClass() + "  onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i("activity_lifecycle", this.getClass() + "  onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i("activity_lifecycle", this.getClass() + "  onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("activity_lifecycle", this.getClass() + "  onDestroy");
+        cancelLoadingDialog();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        Log.i("activity_lifecycle", this.getClass() + "  onNewIntent");
+        super.onNewIntent(intent);
+        setIntent(intent);// 在处理extra之前一定要调用,如果没有调用此句，在getIntent时可能拿不到数据
+        processExtraData();
+    }
+
+    /**
+     * 当launchMode为singleTask时或其他情况时onCreate并不执行，
+     * 因此最好覆盖此方法处理intent传值
+     */
+    protected  void processExtraData(){}
+
+    /**
+     * 初始化加载对话框
+     */
+    private void initLoadingDialog() {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.setMessage("请稍候...");
+    }
+
+    /**
+     * 设置全屏，需在setContentView之前调用
+     */
+    public void setFullScreen(){
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    /**
+     * 获得屏幕宽度
+     * @return
+     */
+    public int getScreenWidth(){
+        Resources resources = this.getResources();
+        DisplayMetrics dm = resources.getDisplayMetrics();
+        return  dm.widthPixels;
+    }
+
+    /**
+     * 获得屏幕高度
+     * @return
+     */
+    public int getScreenHeight(){
+        Resources resources = this.getResources();
+        DisplayMetrics dm = resources.getDisplayMetrics();
+        return  dm.heightPixels;
+    }
+
+
+    /**
+     * 显示加载对话框
+     */
+    public void showLoadingDialog() {
+        showLoadingDialog("请稍候...");
+    }
+
+
+
+    /**
+     * 显示加载对话框
+     *
+     * @param hint 提示信息
+     */
+    public void showLoadingDialog(final String hint) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (progressDialog != null && !progressDialog.isShowing()) {
+                    progressDialog.setMessage(hint);
+                    progressDialog.show();
+                }
+            }
+        });
+    }
+
+    /**
+     * 取消加载对话框
+     */
+    public void cancelLoadingDialog() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (progressDialog != null && progressDialog.isShowing()) {
+                    progressDialog.cancel();
+                }
+            }
+        });
+
+    }
+
+
+    /**
+     * 显示Toast，以长时间显示
+     *
+     * @param msg 显示的内容（字符串）
+     */
+    public void showLongToast(final String msg) {
+        if (!TextUtils.isEmpty(msg)) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (longToast == null) {
+                        longToast = Toast.makeText(getApplicationContext(), msg,
+                                Toast.LENGTH_LONG);
+                    } else {
+                        longToast.setText(msg);
+                    }
+
+                    longToast.show();
+
+                }
+
+            });
+        }
+    }
+
+    /**
+     * 显示Toast，以长时间显示
+     *
+     * @param resId 显示的内容id
+     */
+    public void showLongToast(final int resId) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (longToast == null) {
+                    longToast = Toast.makeText(
+                            BaseActivity.this.getApplicationContext(), resId,
+                            Toast.LENGTH_LONG);
+                } else {
+                    longToast.setText(resId);
+                }
+                longToast.show();
+            }
+        });
+    }
+
+    /**
+     * 显示Toast，以短时间显示
+     *
+     * @param msg 显示的内容（字符串）
+     */
+    public void showShortToast(final String msg) {
+        if (!TextUtils.isEmpty(msg)) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (shortToast == null) {
+                        shortToast = Toast.makeText(getApplicationContext(), msg,
+                                Toast.LENGTH_SHORT);
+                    } else {
+                        shortToast.setText(msg);
+                    }
+
+                    shortToast.show();
+
+                }
+
+            });
+        }
+    }
+
+    /**
+     * 显示Toast，以短时间显示
+     *
+     * @param resId 显示的内容id
+     */
+    public void showShortToast(final int resId) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (shortToast == null) {
+                    shortToast = Toast.makeText(
+                            BaseActivity.this.getApplicationContext(), resId,
+                            Toast.LENGTH_SHORT);
+                } else {
+                    shortToast.setText(resId);
+                }
+                shortToast.show();
+            }
+        });
+    }
+
+    public void back(View v) {
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        cancelLoadingDialog();
+        super.onBackPressed();
+    }
+
+    /**
+     * 跳转到Activity
+     * @param clas              Activity
+     * @param isFinishCurrent   是否Finish当前Activity
+     */
+    public void gotoActivity(Class clas,boolean isFinishCurrent){
+        Intent intent=new Intent(this,clas);
+        startActivity(intent);
+        if(isFinishCurrent){
+            finish();
+        }
+    }
+    /**
+     * 跳转到Activity,保留当前Activity
+     * @param clas              Activity
+     */
+    public void gotoActivity(Class clas){
+        gotoActivity(clas,false);
+    }
+}
---
+++ baselibrary/src/main/java/com/junmeng/base/fragment/BaseFragment.java
@@ -0,0 +1,207 @@
+package com.junmeng.base.fragment;
+
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+
+
+public abstract class BaseFragment extends Fragment {
+
+    private ProgressDialog progressDialog;
+    /**
+     * 显示加载对话框
+     */
+    public void showLoadingDialog() {
+        showLoadingDialog("请稍候...");
+
+    }
+
+    /**
+     * 显示加载对话框
+     *
+     * @param hint 提示信息
+     */
+    public void showLoadingDialog(final String hint) {
+        if (getActivity() != null) {
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (progressDialog != null && !progressDialog.isShowing()) {
+                        progressDialog.setMessage(hint);
+                        progressDialog.show();
+                    } else {
+                        progressDialog = new ProgressDialog(getActivity());
+                        progressDialog.setCanceledOnTouchOutside(false);
+                        progressDialog.setMessage(hint);
+                        progressDialog.show();
+                    }
+                }
+            });
+        }
+
+    }
+
+    /**
+     * 隐藏加载对话框
+     */
+    public void cancelLoadingDialog() {
+        if (getActivity() != null) {
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (progressDialog != null && progressDialog.isShowing()) {
+                        progressDialog.cancel();
+                    }
+                }
+            });
+        }
+    }
+
+    public void showLongToast(final String msg) {
+        if (!TextUtils.isEmpty(msg)) {
+            if (getActivity() != null) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getActivity(), msg, Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        }
+
+    }
+
+    public void showLongToast(final int resId) {
+        if (getActivity() != null) {
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getActivity(), resId, Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+
+    public void showShortToast(final String msg) {
+        if (!TextUtils.isEmpty(msg)) {
+            if (getActivity() != null) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getActivity(), msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
+    }
+
+    public void showShortToast(final int resId) {
+        if (getActivity() != null) {
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getActivity(), resId, Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+
+    /**
+     * 获得屏幕宽度
+     * @return
+     */
+    public int getScreenWidth(){
+        Resources resources = this.getResources();
+        DisplayMetrics dm = resources.getDisplayMetrics();
+        return  dm.widthPixels;
+    }
+
+    /**
+     * 获得屏幕高度
+     * @return
+     */
+    public int getScreenHeight(){
+        Resources resources = this.getResources();
+        DisplayMetrics dm = resources.getDisplayMetrics();
+        return  dm.heightPixels;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        Log.i("fragment_lifecycle", getClass() + "   onAttach");
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i("fragment_lifecycle", getClass() + "   onCreate");
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.i("fragment_lifecycle", getClass() + "   onCreateView");
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        Log.i("fragment_lifecycle", getClass() + "   onActivityCreated");
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        Log.i("fragment_lifecycle", getClass() + "   onStart");
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        Log.i("fragment_lifecycle", getClass() + "   onResume");
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        Log.i("fragment_lifecycle", getClass() + "   onPause");
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.i("fragment_lifecycle", getClass() + "   onStop");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i("fragment_lifecycle", getClass() + "   onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDestroyView() {
+        Log.i("fragment_lifecycle", getClass() + "   onDestroyView");
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDetach() {
+        Log.i("fragment_lifecycle", getClass() + "   onDetach");
+        super.onDetach();
+    }
+}
